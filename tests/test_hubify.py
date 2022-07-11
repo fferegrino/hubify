1181e26dad227ee2dc4b059314d3a4308c582a37
Antonio Feregrino <antonio.feregrino@gmail.com>
2022-07-11 17:04:19 +0100

    Add tests for split function
    Add makefile too

--- tests/test_hubify.py
+++ tests/test_hubify.py
@@ -1,6 +1,8 @@
+from datetime import datetime
+
 import pandas as pd
 
-from hubify.hubify import calculate_continuous_week
+from hubify.hubify import calculate_continuous_week, prepare_time_series
 
 
 def test_simple_week_number():
@@ -50,3 +52,33 @@ def test_muli_year_week_number():
     actual_series = calculate_continuous_week(input_frame)
 
     pd.testing.assert_series_equal(expected_series, actual_series, check_names=False)
+
+
+def test_prepare_time_series():
+
+    # Prepare
+    input_data = pd.Series(
+        [
+            datetime(2022, 1, 3, 10, 20),
+            datetime(2022, 1, 4, 23, 55),
+            datetime(2022, 1, 5, 9, 5),
+            datetime(2022, 1, 6),
+            datetime(2022, 1, 3),
+        ]
+    )
+
+    expected = pd.DataFrame(
+        [
+            (datetime(2022, 1, 3), 2, 0, 1, 2022),
+            (datetime(2022, 1, 4), 1, 1, 1, 2022),
+            (datetime(2022, 1, 5), 1, 2, 1, 2022),
+            (datetime(2022, 1, 6), 1, 3, 1, 2022),
+        ],
+        columns=["date", "events", "weekday", "week", "year"],
+    )
+
+    # Act
+    actual_result = prepare_time_series(input_data)
+
+    # Assert
+    pd.testing.assert_frame_equal(actual_result, expected)
